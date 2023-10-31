f5286418d2590e089b60877c8f93065796b09ed9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-31 13:28:04 -0300

    at

--- src/garbage/garbage.h
+++ src/garbage/garbage.h
@@ -2,4 +2,19 @@
 
 typedef  struct UniversalGarbage{
 
-};
+    void (*clear_callback)(short type, void*value);
+    void *main_return;
+    void **values;
+    long size;
+
+}UniversalGarbage;
+
+UniversalGarbage newUniversalGarbage(UniversalGarbage *self,void (*clear_callback)(short type, void*value));
+
+void UniversalGarbage_add_normal_value(UniversalGarbage *self,void *value);
+
+void UniversalGarbage_add_especial_value(UniversalGarbage *self,short type,void *value);
+
+void UniversalGarbage_free_including_return(UniversalGarbage *self);
+
+void UniversalGarbage_free(UniversalGarbage *self);
