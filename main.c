c2f45ea367f8e4c5b53a78fa055c0a98b67389b7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-01 20:28:01 -0300

    att

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 int main(){
     UniversalGarbage  *garbage = newUniversalGarbage(NULL);
 
-    char *test = strdup("aaaaaaaa");
+    char *test = NULL;
     UniversalGarbage_add_simple_value(garbage,(void **)&test);
+
+    test = strdup("sssssssssssssss");
     printf("test %s\n",test);
+
 
     UniversalGarbage_free(garbage);
     return 0;
