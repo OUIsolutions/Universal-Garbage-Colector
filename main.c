dc35fe641569e511d00de66e82f312380ec901a3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-01 18:55:43 -0300

    att

--- main.c
+++ main.c
@@ -5,23 +5,22 @@
 int main(){
     UniversalGarbage  *garbage = newUniversalGarbage(NULL);
 
-    char *final_string = NULL;
+    char *final_string = calloc(1,sizeof(char));
+    UniversalGarbage_add_simple_value(garbage,final_string);
+
+    char *new_string = NULL;
+
     for(int i = 0; i < 100; i++){
 
-        char *new_string = NULL;
+        garbage->old_value = new_string;
+        new_string  = calloc(20,sizeof(char));
+        UniversalGarbage_add_simple_value(garbage,new_string);
+        sprintf(new_string,"%d\n",i);
+        final_string = realloc(final_string,
+                               (strlen(final_string)+ strlen(new_string) + 2) *sizeof(char)
+        );
 
-        if(final_string){
-            new_string  = calloc(strlen(final_string) + 20,sizeof(char));
-            UniversalGarbage_add_simple_value(garbage,new_string);
-            sprintf(new_string,"%s%d\n",final_string,i);
-        }
-
-        if(!final_string){
-            new_string= calloc(20,sizeof(char));
-            UniversalGarbage_add_simple_value(garbage,new_string);
-            sprintf(new_string,"%d\n",i);
-        }
-        final_string = new_string;
+        strcat(final_string,new_string);
     }
     printf("final %s\n",final_string);
     UniversalGarbage_free(garbage);
