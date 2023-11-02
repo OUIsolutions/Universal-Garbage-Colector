d66eff0a5957ddfd8ef78603f819fcc5b6c4d036
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-01 22:08:07 -0300

    att

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ int main(){
     UniversalGarbage  *garbage = newUniversalGarbage(NULL);
 
     char *final_string = calloc(1,sizeof(char));
-    UniversalGarbage_add_or_resset_simple_value(garbage, (void **) &final_string);
+    UniversalGarbage_add_or_resset_simple_value(garbage, UniversalCast(final_string));
 
     char *new_string = NULL;
 
     for(int i = 0; i < 100; i++){
 
         new_string  = calloc(20,sizeof(char));
-        UniversalGarbage_add_or_resset_simple_value(garbage, (void **) &new_string);
+        UniversalGarbage_add_or_resset_simple_value(garbage, UniversalCast(new_string));
         sprintf(new_string,"%d\n",i);
 
         final_string =realloc(final_string,
                    (strlen(final_string)+ strlen(new_string) + 2) *sizeof(char)
         );
-        UniversalGarbage_reallocate_simple_value(garbage,(void**)&final_string);
+        UniversalGarbage_reallocate_simple_value(garbage, UniversalCast(final_string));
 
         strcat(final_string,new_string);
     }
--- src/macros.h
+++ src/macros.h
@@ -2,3 +2,5 @@
 #define UniversalGarbage_create_empty_struct(name,element_type) \
 (element_type*)malloc(sizeof(element_type));    \
 *name = (element_type){0};
+
+#define UniversalCast(pointer) (void**)&pointer
