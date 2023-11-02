c96ab4e8a2ea546292c5cc38b07fc0a932accb10
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-01 23:34:37 -0300

    at

--- main.c
+++ main.c
@@ -5,19 +5,18 @@
 
 
 int main(){
-    UniversalGarbage  *garbage = newUniversalGarbage(NULL);
+    UniversalGarbage  *garbage = newUniversalGarbage();
 
 
-    
     char *final_string = (char*)calloc(1,sizeof(char));
-    UniversalGarbage_add_or_resset(garbage, UniversalCast(final_string));
+    UniversalGarbage_add_or_resset(garbage,free, UniversalCast(final_string));
 
     char *new_string = NULL;
 
     for(int i = 0; i < 100; i++){
 
         new_string  = (char*)calloc(20,sizeof(char));
-        UniversalGarbage_add_or_resset(garbage, UniversalCast(new_string));
+        UniversalGarbage_add_or_resset(garbage,free, UniversalCast(new_string));
         sprintf(new_string,"%d\n",i);
 
         final_string =(char*)realloc(final_string,
