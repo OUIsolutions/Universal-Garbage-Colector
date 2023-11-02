7dccd1b424d8c25e46afa8d0a7560a501c5a5926
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-02 05:12:38 -0300

    at

--- main.c
+++ main.c
@@ -1,45 +1,10 @@
 
 #include "src/one.h"
 
-typedef struct Car{
-    char *name;
-    char *color;
-    UniversalGarbage  *garbage;
-}Car;
-
-Car *newCar(){
-    Car *self = UniversalGarbage_create_empty_struct(self,Car);
-    self->garbage = newUniversalGarbage();
-    UniversalGarbage_add(self->garbage,free,self);
-    UniversalGarbage_add(self->garbage,free,self->name);
-    UniversalGarbage_add(self->garbage, free, self->color);
-    return self;
-
-}
-
-void Car_set_name(Car*self,const char *name){
-    self->name = strdup(name);
-    UniversalGarbage_resset(self->garbage,self->name);
-}
-
-void Car_set_color(Car*self,const char *color){
-    self->color = strdup(color);
-    UniversalGarbage_resset(self->garbage,self->color);
-}
-void Car_free(Car *self){
-    UniversalGarbage_free(self->garbage);
-}
-
 
 int main(){
     UniversalGarbage  *garbage = newUniversalGarbage();
-    Car  *test = newCar();
-    UniversalGarbage_add(garbage,Car_free,test);
-    Car_set_name(test,"ferrari");
-    Car_set_color(test,"red");
-
-    printf("model:%s\n",test->name);
-    printf("color:%s\n",test->color);
+    char * value_a = strdup("test");
 
 
     UniversalGarbage_free(garbage);
--- src/macros.h
+++ src/macros.h
@@ -9,8 +9,16 @@
 #define UniversalGarbage_set_return(garbage,deallocator_callback,value) \
         rawUniversalGarbage_set_return(garbage,(void*)deallocator_callback,UniversalGarbage_cast(value))
 
+#define UniversalGarbage_set_simple_return(garbage,value) \
+    UniversalGarbage_set_return(garbage,free,value)
+
+
 #define UniversalGarbage_add(garbage,deallocator_callback,value) \
     rawUniversalGarbage_add(garbage,(void*)deallocator_callback,UniversalGarbage_cast(value))
+
+#define UniversalGarbage_add_simple(garbage,value) \
+     UniversalGarbage_add(garbage,free,value)
+
 
 #define UniversalGarbage_reallocate(garbage,value) \
     rawUniversalGarbage_reallocate(garbage,UniversalGarbage_cast(value))
