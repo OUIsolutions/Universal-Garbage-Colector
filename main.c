6b1e4ce8f7b145b4ee4bf16e13101a5c48bddfc0
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-01 22:17:15 -0300

    at

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 
 #include "src/one.h"
 
+void test(void (*callback)(void *value)){
+
+}
 
 
 int main(){
     UniversalGarbage  *garbage = newUniversalGarbage(NULL);
 
-    char *final_string = calloc(1,sizeof(char));
+    test((void*)(void*)UniversalGarbage_free);
+
+    
+    char *final_string = (char*)calloc(1,sizeof(char));
     UniversalGarbage_add_or_resset_simple_value(garbage, UniversalCast(final_string));
 
     char *new_string = NULL;
 
     for(int i = 0; i < 100; i++){
 
-        new_string  = calloc(20,sizeof(char));
+        new_string  = (char*)calloc(20,sizeof(char));
         UniversalGarbage_add_or_resset_simple_value(garbage, UniversalCast(new_string));
         sprintf(new_string,"%d\n",i);
 
-        final_string =realloc(final_string,
+        final_string =(char*)realloc(final_string,
                    (strlen(final_string)+ strlen(new_string) + 2) *sizeof(char)
         );
         UniversalGarbage_reallocate_simple_value(garbage, UniversalCast(final_string));
