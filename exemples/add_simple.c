dbf76d4c745bc1ba11f54df1dd8ea013b61895e7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-02 21:07:04 -0300

    att

---
+++ exemples/add_simple.c
@@ -0,0 +1,14 @@
+
+#include "UniversalGarbage.h"
+
+
+int main(){
+    UniversalGarbage *garbage = newUniversalGarbage();
+
+    char *test = strdup("test content");
+    UniversalGarbage_add_simple(garbage,test);
+    printf("test value:%s\n",test);
+
+    UniversalGarbage_free(garbage);
+}
+
