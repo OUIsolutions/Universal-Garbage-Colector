12fb0f8fd55b62a93cde2b00765e8948d3690caa
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-28 20:13:02 -0300

    att

--- teste.c
+++ teste.c
@@ -1,32 +1,15 @@
-#include "src/declaration.h"
 
-typedef struct Car{
-    char *name;
-    char *color;
-    UniversalGarbage  *garbage;
-}Car;
-
-Car *newCar();
-
-
-void Car_set_name(Car*self,const char *name);
-
-
-void Car_set_color(Car*self,const char *color);
-
-void Car_free(Car *self);
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "release/UniversalGarbageOne.c"
 
 int main(){
     UniversalGarbage *garbage = newUniversalGarbage();
-    Car  *test = newCar();
-    //we set Car_free as the dealocator callback
-    UniversalGarbage_add(garbage, Car_free,test);
-    Car_set_name(test,"ferrari");
-    Car_set_color(test,"red");
 
-    printf("model:%s\n",test->name);
-    printf("color:%s\n",test->color);
+    char *test = strdup("test content");
+    UniversalGarbage_add_simple(garbage,test);
+    printf("test value:%s\n",test);
+
     UniversalGarbage_free(garbage);
 }
