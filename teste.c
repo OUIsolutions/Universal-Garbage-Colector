2c0e3dac9911e8b6ac5443f3b9014d31e390bae4
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-28 20:19:17 -0300

    att

--- teste.c
+++ teste.c
@@ -1,8 +1,5 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "release/UniversalGarbageOne.c"
+#include "src/one.c"
 
 int main(){
     UniversalGarbage *garbage = newUniversalGarbage();
